a6fb6106e4c85bc2d209c34042da3314fe146ea5
apple <appleapple@apples-macbook-air.local>
2018-11-03 23:58:03 +0530

    Initial Commit

---
+++ Prac1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Prac1
+//
+//  Created by apple on 11/3/18.
+//  Copyright © 2018 apple. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
